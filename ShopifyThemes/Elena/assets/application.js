ba019f5a18df54202115fb3c68424f44d2a9f8f5
las348 <las348@nyu.edu>
2021-07-18 17:31:29 -0400

    login page

--- ShopifyThemes/Elena/assets/application.js
+++ ShopifyThemes/Elena/assets/application.js
@@ -29,3 +29,14 @@ if( document.getElementById('AddressCountryNew') != null ) {
         provinceSelector.innerHTML = options;
     });
 }
+
+if(document.getElementById("forgotPassword") != null) {
+    document.getElementById("forgotPassword").addEventListener("click", function(e) {
+        console.log("I clicked");
+        const element = document.querySelector("#forgot_password_form");
+        if(element.classList.contains("d-none")) {
+            element.classList.remove("d-none");
+            element.classList.add("d-block");
+        }
+    });
+}
