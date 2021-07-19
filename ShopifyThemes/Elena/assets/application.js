4a90f9a07a08aa2db5a8066aeb8dbbc07b482a88
las348 <las348@nyu.edu>
2021-07-18 20:08:10 -0400

    catalog product modal

--- ShopifyThemes/Elena/assets/application.js
+++ ShopifyThemes/Elena/assets/application.js
@@ -40,3 +40,56 @@ if(document.getElementById("forgotPassword") != null) {
         }
     });
 }
+
+var localeItems = document.querySelectorAll("#localeItem");
+if(localeItems.length > 0) {
+    localeItems.forEach(item => {
+        item.addEventListener("click", event => {
+            document.getElementById("localeCode").value = item.getAttribute("lang");
+            document.getElementById("localization_form_tag").submit();
+        });
+    });
+}
+
+var productInfoAnchors = document.querySelectorAll("#productInfoAnchor");
+
+var productModal;
+
+if( document.getElementById('productInfoModal') != null ) {
+    productModal = new bootstrap.Modal(document.getElementById('productInfoModal'), {});
+}
+
+if(productInfoAnchors.length > 0) {
+    productInfoAnchors.forEach(item => {
+        item.addEventListener("click", event => {
+            
+            var url = '/products/' + item.getAttribute('product-handle') + '.js';
+
+            fetch(url)
+            .then((resp) => resp.json())
+            .then(function(data) {
+                console.log(data);
+
+                document.getElementById("productInfoImg").src = data.images[0];
+                document.getElementById("productInfoTitle").innerHTML = data.title;
+                document.getElementById("productInfoPrice").innerHTML = item.getAttribute('product-price');
+                document.getElementById("productInfoDescription").innerHTML = data.description;
+
+                var variants = data.variants;
+                var variantSelect = document.getElementById("modalItemID");
+
+                variantSelect.innerHTML = '';
+
+                variants.forEach(function( variant, index) {
+                    console.log(variant);
+
+                    variantSelect.options[variantSelect.options.length] = new Option(variant.option1, variant.id);
+                });
+
+                productModal.show();
+            });
+
+            
+        });
+    });
+}
